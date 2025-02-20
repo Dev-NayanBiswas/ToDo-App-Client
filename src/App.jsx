d0629c5a6d3ce528acc17596bf5a6197d7c326bf
Dev-NayanBiswas <dev.nayan.biswas@gmail.com>
2025-02-20 16:43:25 +0600

    connecting to Github repo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <section>
+        <h1 className="text-5xl font-semibold text-center text-green-500">ToDo App</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
